de607254c5ac855d928632a37abc352a5d0f20f6
ocimara <ocimarafbarcellos@gmail.com>
2018-03-19 22:59:22 -0300

    mais um

--- app/src/main/java/br/com/ocimara/fakezap/ViewPagerAdapter.kt
+++ app/src/main/java/br/com/ocimara/fakezap/ViewPagerAdapter.kt
@@ -1,5 +1,34 @@
 package br.com.ocimara.fakezap
 
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
 /**
  * Created by logonrm on 19/03/2018.
  */
+
+class ViewPagerAdapter(fragmentManager: FragmentManager)
+    : FragmentPagerAdapter(fragmentManager) {
+
+    val meusFragments = ArrayList<Fragment>()
+
+
+    override fun getItem(position: Int): Fragment {
+        return meusFragments[position]
+        //ou também posso fazer assim
+        //return meusFragments.get(position)
+    }
+
+    override fun getCount(): Int {
+        return meusFragments.size
+
+    }
+
+    fun adicionar(fragment: Fragment){
+        meusFragments.add(fragment)
+
+    }
+
+
+}
